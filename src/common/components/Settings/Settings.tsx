4308846254bfe40dd8ed7468898410fe03f06d21
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-29 02:39:54 +0300

    Add initial Settings component
    Introduced a new Settings component file in the common components directory. The component is currently a functional component typed with SettingsProps but contains only a basic structure without functionality or styling.

---
+++ src/common/components/Settings/Settings.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import {SettingsProps} from "common/types/Settings/SettingsProps.ts";
+
+export const Settings: FC<SettingsProps> = () => {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
