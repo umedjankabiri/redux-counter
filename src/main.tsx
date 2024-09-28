c49264b2569b97d79d021c9e1b3e487c4226ca40
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-29 00:57:30 +0300

    Fix import path in main.tsx
    Changed the import path for App component from a relative to an absolute path. This ensures consistency with other imports and resolves path resolution issues.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from 'src/App/App.tsx'
 import './index.css'
+import App from "App/App.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
