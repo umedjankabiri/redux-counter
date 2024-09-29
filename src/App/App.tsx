0521fedd035b6d3bd504f40d7c69dd50a4483bcd
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-29 03:23:47 +0300

    Add Settings component to App
    Include the Settings component in the App to enhance the user interface. This change will facilitate accessing the settings page directly from the main application.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,12 @@
 import 'App/App.css'
+import {Settings} from "features/ui/Settings/Settings.tsx";
 
 function App() {
 
   return (
       <>
         <h1>Start redux counter project</h1>
+          <Settings/>
       </>
   )
 }
