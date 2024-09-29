fb5a3bad48a3b997d14ee99977967575fe66ce6b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-09-29 03:23:25 +0300

    Refactor Settings component and update structure
    Renamed Settings component path to better reflect its role. Added styles and integrated EnterValue and Button components for improved functionality. These changes enhance the component's layout and usability.

---
+++ src/features/ui/Settings/Settings.tsx
@@ -0,0 +1,30 @@
+import {FC} from "react";
+import {SettingsProps} from "common/types/Settings/SettingsProps.ts";
+import stl from "./Settings.module.css"
+import {EnterValue} from "common/components/EnterValue/EnterValue.tsx";
+import {Button} from "common/components/Button/Button.tsx";
+
+export const Settings: FC<SettingsProps> = () => {
+    let defaultValue = 0
+
+    return (
+        <div className={stl.settingsWrapper}>
+            <div className={stl.values}>
+                <div className={stl.maxValue}>
+                    <span>Enter max value: </span>
+                    <EnterValue value={defaultValue} onChange={() => {
+                    }}/>
+                </div>
+                <div className={stl.startValue}>
+                    <span>Enter start value: </span>
+                    <EnterValue value={defaultValue} onChange={() => {
+                    }}/>
+                </div>
+            </div>
+            <div className={stl.buttonsWrapper}>
+                <Button>Enter</Button>
+                <Button>Clear</Button>
+            </div>
+        </div>
+    )
+}
