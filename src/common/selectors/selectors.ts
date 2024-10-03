aed980822d1654ff06714c29b318e0d03f022ae5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 01:12:49 +0300

    Add selector for settings state
    Implemented a new selector `selectSettings` to retrieve `SettingsProps` from the Redux store. This complements the existing `selectCounter` selector, ensuring both counter and settings states are accessible.

--- src/common/selectors/selectors.ts
+++ src/common/selectors/selectors.ts
@@ -1,4 +1,6 @@
 import {CounterProps} from "common/types/CounterReducer/CounterProps.ts";
 import {RootState} from "App/store.ts";
+import {SettingsProps} from "common/types/Settings/SettingsProps.ts";
 
 export const selectCounter = (state: RootState): CounterProps => state.counter
+export const selectSettings = (state: RootState): SettingsProps => state.settings
