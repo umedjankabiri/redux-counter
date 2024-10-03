0749360aaeb1721fbbcdb72a8bd6b6c3eea267a7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-03 20:54:31 +0300

    Add startValue and maxValue to counterReducer
    Updated the initial state and reducer to include startValue and maxValue. Added new action types and associated action creators to manage these properties. This enhances the counter functionality by allowing a configurable starting and maximum value.

--- src/features/model/counterReducer/counterReducer.ts
+++ src/features/model/counterReducer/counterReducer.ts
@@ -1,11 +1,13 @@
-import {StateProps} from "common/types/CounterReducer/StateProps.ts";
+import {CounterProps} from "common/types/CounterReducer/CounterProps.ts";
 import {ActionProps} from "common/types/CounterReducer/ActionProps.ts";
 
-const initialState: StateProps = {
-    counter: 0
+const initialState: CounterProps = {
+    counter: 0,
+    startValue: 0,
+    maxValue: 0
 }
 
-export const counterReducer = (state: StateProps = initialState, action: ActionProps): StateProps => {
+export const counterReducer = (state: CounterProps = initialState, action: ActionProps): CounterProps => {
     switch (action.type) {
         case "INCREMENT":
             return {
@@ -17,10 +19,26 @@ export const counterReducer = (state: StateProps = initialState, action: ActionP
                 ...state,
                 counter: action.payload.counter - 1
             }
+        case "START-VALUE":
+            return {
+                ...state,
+                startValue: action.payload.startValue
+            }
+        case "MAX-VALUE":
+            return {
+                ...state,
+                maxValue: action.payload.maxValue
+            }
         default:
             return state
     }
 }
 
-export const incrementCounter = (counter: number) => ({type: "INCREMENT", payload: {counter: counter}}) as const
-export const decrementCounter = (counter: number) => ({type: "DECREMENT", payload: {counter: counter}}) as const
+export const incrementCounter = (counter: number) =>
+    ({type: "INCREMENT", payload: {counter: counter}}) as const
+export const decrementCounter = (counter: number) =>
+    ({type: "DECREMENT", payload: {counter: counter}}) as const
+export const startValue = (startValue: number) =>
+    ({type: "START-VALUE", payload: {startValue: startValue}}) as const
+export const maxValue = (maxValue: number) =>
+    ({type: "MAX-VALUE", payload: {maxValue: maxValue}}) as const
