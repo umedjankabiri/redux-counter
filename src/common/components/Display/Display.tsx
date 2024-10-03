2ba40c5d012f3cfd2e1a5c81d6089cfb619ac6f5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-03 20:51:49 +0300

    Refactor Display component to use children prop
    Changed the Display component to render its children prop instead of the showCounter prop. This allows for greater flexibility in what can be displayed within the component, making it more reusable across different contexts.

--- src/common/components/Display/Display.tsx
+++ src/common/components/Display/Display.tsx
@@ -1,9 +1,9 @@
 import {FC} from "react";
 import {DisplayProps} from "common/types/Display/DisplayProps.ts";
 
-export const Display: FC<DisplayProps> = (props) => {
+export const Display: FC<DisplayProps>= (props) => {
 
     return (
-        <h3 className={props.className}>{props.showCounter}</h3>
+        <h3 className={props.className}>{props.children}</h3>
     )
 }
