bef2f7c940e90b5bc78814fb7749293c77539a77
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 00:55:14 +0300

    Wrap App component with BrowserRouter
    This change introduces React Router to enable client-side routing in the application. By wrapping the App component with BrowserRouter, we are setting up the foundational routing mechanism for future route configurations.

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import './index.css'
 import App from "App/App.tsx";
 import {Provider} from "react-redux";
 import {store} from "App/store.ts";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <App/>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </Provider>
 )
