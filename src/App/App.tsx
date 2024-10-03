29f381d71efb8c9b15e281091f4d28b2940d5fed
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 00:55:31 +0300

    Add navigation and routing to App component
    Refactored the App component to include navigation links with `NavLink` and integrated routing using `Routes` and `Route` from "react-router-dom". Introduced `Error404` component for unrecognized paths and shifted CSS import to CSS modules for scoped styling.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,36 @@
-import 'App/App.css'
+import 'App/App.module.css'
+import stl from "App/App.module.css"
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Settings} from "features/ui/Settings/Settings.tsx";
 import {Counter} from "features/ui/Counter/Counter.tsx";
+import {Error404} from "common/components/Error404/Error404.tsx";
+import {PATH} from "common/utils/path.ts";
 
 function App() {
 
-  return (
-      <>
-        <h1>Start redux counter project</h1>
-          <Settings/>
-          <Counter/>
-      </>
-  )
+    return (
+        <div className={stl.mainWrapper}>
+            <div className={stl.navigation}>
+                <div>
+                    <NavLink className={({isActive}) =>
+                        isActive ? stl.activeLink : stl.link} to={PATH.SETTINGS}
+                    >SETTINGS</NavLink>
+                </div>
+                <div>
+                    <NavLink className={({isActive}) =>
+                        isActive ? stl.activeLink : stl.link} to={PATH.COUNTER}
+                    >COUNTER</NavLink>
+                </div>
+            </div>
+            <Routes>
+                <Route path={"/"} element={<Navigate to={PATH.SETTINGS}/>}/>
+                <Route path={PATH.SETTINGS} element={<Settings/>}/>
+                <Route path={PATH.COUNTER} element={<Counter/>}/>
+                <Route path={PATH.ERROR404} element={<Error404/>}/>
+                <Route path={"/*"} element={<Error404/>}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default App
