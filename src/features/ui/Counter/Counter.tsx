46578c8ded393580960f8c9268b9567ea9826ce4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 06:20:19 +0300

    Add settings button and disable logic to Counter component
    Implemented a 'Settings' button to navigate back to the settings page. Added logic to disable 'Plus', 'Minus', and 'Clear' buttons based on counter values to prevent invalid actions. Updated action creators to align with new naming conventions.

--- src/features/ui/Counter/Counter.tsx
+++ src/features/ui/Counter/Counter.tsx
@@ -1,22 +1,31 @@
 import {FC} from "react";
 import stl from "./Counter.module.css"
 import {useRootSelector} from "common/hooks/useRootSelector.ts";
-import {selectCounter} from "common/selectors/selectors.ts";
+import {selectCounter, selectSettings} from "common/selectors/selectors.ts";
 import {Display} from "common/components/Display/Display.tsx";
 import {Button} from "common/components/Button/Button.tsx";
 import {useRootDispatch} from "common/hooks/useRootDispatch.ts";
-import {clearCounter, decrementCounter, incrementCounter} from "features/model/counterReducer/counterReducer.ts";
+import {clearCounterAC, decrementCounterAC, incrementCounterAC} from "features/model/counterReducer/counterReducer.ts";
+import {useNavigate} from "react-router-dom";
+import {PATH} from "common/utils/path.ts";
 
 export const Counter: FC = () => {
     const {counter} = useRootSelector(selectCounter)
+    const {startValue, maxValue} = useRootSelector(selectSettings)
     const dispatch = useRootDispatch()
+    const navigate = useNavigate()
 
     const handlePlusClick = () =>
-        dispatch(incrementCounter(counter))
+        dispatch(incrementCounterAC(counter))
     const handleMinusClick = () =>
-        dispatch(decrementCounter(counter))
+        dispatch(decrementCounterAC(counter))
     const handleClearClick = () =>
-        dispatch(clearCounter(counter))
+        dispatch(clearCounterAC(startValue))
+    const backHandler = () => navigate(PATH.SETTINGS)
+
+    const disablePlus = maxValue == counter
+    const disableMinus = counter == startValue
+    const disableClear = counter == startValue
 
     return (
         <div className={stl.counterWrapper}>
@@ -24,9 +33,12 @@ export const Counter: FC = () => {
                 <Display className={stl.display}>{counter}</Display>
             </div>
             <div className={stl.buttonsWrapper}>
-                <Button className={stl.plus} onClick={handlePlusClick}>Plus</Button>
-                <Button className={stl.minus} onClick={handleMinusClick}>Minus</Button>
-                <Button className={stl.clear} onClick={handleClearClick}>Clear</Button>
+                <Button className={stl.plus} onClick={handlePlusClick} disabled={disablePlus}>Plus</Button>
+                <Button className={stl.minus} onClick={handleMinusClick} disabled={disableMinus}>Minus</Button>
+                <Button className={stl.clear} onClick={handleClearClick} disabled={disableClear}>Clear</Button>
+            </div>
+            <div>
+                <Button className={stl.back} onClick={backHandler}>Settings</Button>
             </div>
         </div>
     )
