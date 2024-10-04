56d4ef9434c794e24b4b771e1a6dbf09eb13b819
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-05 02:27:50 +0300

    Add selectors for error and message states
    Included selectors for the error and messages states to enable retrieving error and message data from the Redux store. This update enhances state management by allowing components to access and display error and message information more efficiently.

--- src/common/selectors/selectors.ts
+++ src/common/selectors/selectors.ts
@@ -1,6 +1,10 @@
 import {CounterProps} from "common/types/CounterReducer/CounterProps.ts";
 import {RootState} from "App/store.ts";
 import {SettingsProps} from "common/types/SettingsReducer/SettingsProps.ts";
+import {ErrorProps} from "common/types/ErrorReducer/ErrorProps.ts";
+import {MessagesProps} from "common/types/MessageReducer/MessagesProps.ts";
 
 export const selectCounter = (state: RootState): CounterProps => state.counter
 export const selectSettings = (state: RootState): SettingsProps => state.settings
+export const selectError = (state: RootState): ErrorProps => state.error
+export const selectMessage = (state: RootState): MessagesProps => state.messages
