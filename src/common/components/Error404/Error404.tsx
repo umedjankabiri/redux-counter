77c1ccb444c1e600af52df5e11425802cb05696e
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 06:19:09 +0300

    Enhance Error404 component styling and remove unused variable
    Updated the color of the '404' text to red for better visibility and converted a span to a paragraph for improved semantics. Additionally, removed the unused `originalPath` variable to clean up the code.

--- src/common/components/Error404/Error404.tsx
+++ src/common/components/Error404/Error404.tsx
@@ -1,12 +1,13 @@
 import stl from "common/components/Error404/Error404.module.css"
 
 export const Error404 = () => {
-    const originalPath = window.location.pathname
 
     return (
         <div className={stl.error404}>
-            <h1>ERROR 404</h1>
-            <span>The requested URL: <span className={stl.path}>{originalPath}</span> was not found on this server.</span>
+            <h1>ERROR <span style={{color: "red"}}>404</span></h1>
+            <p>The requested URL:{" "}
+                <span className={stl.path}>{window.location.pathname}</span>{" "}
+                was not found on this server.</p>
         </div>
     )
 }
