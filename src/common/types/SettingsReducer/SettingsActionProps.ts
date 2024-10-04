fc59e286e0df4a96ca69c8aa23042ead93a39b95
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 06:18:05 +0300

    Update action types for SettingsReducer
    Replaced old action creators with new ones and added a new action type for resetting values. This change ensures that the SettingsReducer uses accurate and updated action creators.

--- src/common/types/SettingsReducer/SettingsActionProps.ts
+++ src/common/types/SettingsReducer/SettingsActionProps.ts
@@ -1,5 +1,9 @@
-import {maxValue, startValue} from "features/model/settingsReducer/settingsReducer.ts";
+import {maxValueAC, resetValueAC, startValueAC} from "features/model/settingsReducer/settingsReducer.ts";
 
-type StartValueActionProps = ReturnType<typeof startValue>;
-type MaxValueActionProps = ReturnType<typeof maxValue>;
-export type SettingsActionProps = | StartValueActionProps | MaxValueActionProps
+type StartValueActionProps = ReturnType<typeof startValueAC>;
+type MaxValueActionProps = ReturnType<typeof maxValueAC>;
+type ResetValuesActionProps = ReturnType<typeof resetValueAC>;
+export type SettingsActionProps = | StartValueActionProps
+    | MaxValueActionProps
+    | ResetValuesActionProps
+
