ba8c6cc8c8e001ffeadc07eeb6a980bbcf3052d6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-05 02:28:08 +0300

    Add error and message reducers to store
    Included errorReducer and messageReducer in the rootReducer to manage state related to errors and messages. This enhances the application's state management capabilities by modularizing error and message handling.

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,10 +1,14 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "features/model/counterReducer/counterReducer.ts";
 import {settingsReducer} from "features/model/settingsReducer/settingsReducer.ts";
+import {errorReducer} from "features/model/errorsReducer/errorReducer.ts";
+import {messageReducer} from "features/model/messagesReducer/messageReducer.ts";
 
 const rootReducer = combineReducers({
     counter: counterReducer,
     settings: settingsReducer,
+    error: errorReducer,
+    messages: messageReducer
 })
 export const store = legacy_createStore(rootReducer);
 export type RootState = ReturnType<typeof store.getState>
