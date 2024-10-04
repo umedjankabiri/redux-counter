0dcf906fee703c72cb5931cedc1e0b11f2c79ac7
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 06:19:45 +0300

    Add value prop to EnterValue input component
    This changes ensures the input reflects the value passed via props. It also simplifies the onChange handler by directly using props.onChange.

--- src/common/components/EnterValue/EnterValue.tsx
+++ src/common/components/EnterValue/EnterValue.tsx
@@ -2,9 +2,8 @@ import {FC} from "react";
 import {EnterValueProps} from "common/types/EnterValue/EnterValueProps.ts";
 
 export const EnterValue: FC<EnterValueProps> = (props) => {
-    const onChangeHandler = () => props.onChange
 
     return (
-        <input className={props.className} type="number" onChange={onChangeHandler}/>
+        <input className={props.className} type="number" value={props.value} onChange={props.onChange}/>
     )
 }
