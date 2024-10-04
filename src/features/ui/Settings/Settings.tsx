ee1d3e83772264587e90cf509b89cef0ada93110
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-04 06:18:22 +0300

    Refactor Settings component for state management
    Introduced Redux hooks to manage state updates and navigation actions within the Settings component. Updated event handlers for input changes and button clicks to dispatch corresponding actions.

--- src/features/ui/Settings/Settings.tsx
+++ src/features/ui/Settings/Settings.tsx
@@ -1,27 +1,57 @@
-import {FC} from "react";
-import {SettingsProps} from "common/types/Settings/SettingsProps.ts";
+import {ChangeEvent, FC} from "react";
 import stl from "./Settings.module.css"
 import {EnterValue} from "common/components/EnterValue/EnterValue.tsx";
 import {Button} from "common/components/Button/Button.tsx";
+import {useRootSelector} from "common/hooks/useRootSelector.ts";
+import {selectSettings} from "common/selectors/selectors.ts";
+import {useRootDispatch} from "common/hooks/useRootDispatch.ts";
+import {maxValueAC, resetValueAC, startValueAC} from "features/model/settingsReducer/settingsReducer.ts";
+import {clearCounterAC} from "features/model/counterReducer/counterReducer.ts";
+import {useNavigate} from "react-router-dom";
+import {PATH} from "common/utils/path.ts";
 
-export const Settings: FC<SettingsProps> = () => {
-    let defaultValue = 0
+export const Settings: FC = () => {
+    const {startValue, maxValue} = useRootSelector(selectSettings)
+    const dispatch = useRootDispatch()
+    const navigate = useNavigate()
+
+    const zero = 0
+
+    const onChangeMaxValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const tempMaxValue = +event.currentTarget.value
+
+        dispatch(maxValueAC(tempMaxValue))
+    }
+    const onChangeStartValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const tempStartValue = +event.currentTarget.value
+
+        dispatch(startValueAC(tempStartValue))
+    }
+    const onClickEnterHandler = () => {
+        dispatch(maxValueAC(maxValue))
+        dispatch(startValueAC(startValue))
+        navigate(PATH.COUNTER)
+    }
+    const onClickResetHandler = () => {
+        dispatch(resetValueAC(zero))
+        dispatch(clearCounterAC(zero))
+    }
 
     return (
         <div className={stl.settingsWrapper}>
-            <div className={stl.values}>
+            <div className={stl.valuesWrapper}>
                 <div className={stl.maxValue}>
                     <span>Enter max value: </span>
-                    <EnterValue className={stl.enterValue} value={defaultValue} onChange={() => {}}/>
+                    <EnterValue className={stl.enterValue} value={maxValue} onChange={onChangeMaxValueHandler}/>
                 </div>
                 <div className={stl.startValue}>
                     <span>Enter start value: </span>
-                    <EnterValue className={stl.enterValue} value={defaultValue} onChange={() => {}}/>
+                    <EnterValue className={stl.enterValue} value={startValue} onChange={onChangeStartValueHandler}/>
                 </div>
             </div>
             <div className={stl.buttonsWrapper}>
-                <Button className={stl.enter}>Enter</Button>
-                <Button className={stl.clear}>Reset</Button>
+                <Button className={stl.enter} onClick={onClickEnterHandler}>Enter</Button>
+                <Button className={stl.clear} onClick={onClickResetHandler}>Reset</Button>
             </div>
         </div>
     )
