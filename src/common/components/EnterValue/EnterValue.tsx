f1fc5326660f123477a159931dde23f0f71d1ea2
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-05 23:49:13 +0300

    Add min, max, and disabled props to EnterValue component
    This change enhances the EnterValue component by allowing it to accept min, max, and disabled attributes, enabling better control over the input element. These additions provide more flexibility in validating user input and managing the component's state.

--- src/common/components/EnterValue/EnterValue.tsx
+++ src/common/components/EnterValue/EnterValue.tsx
@@ -4,6 +4,13 @@ import {EnterValueProps} from "common/types/EnterValue/EnterValueProps.ts";
 export const EnterValue: FC<EnterValueProps> = (props) => {
 
     return (
-        <input className={props.className} type="number" value={props.value} onChange={props.onChange}/>
+        <input className={props.className}
+               type="number"
+               min={props.min}
+               max={props.max}
+               disabled={props.disabled}
+               value={props.value}
+               onChange={props.onChange}
+        />
     )
 }
