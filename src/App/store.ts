beb450ae13a60cfd7d5e5ba8d01d0158624439ce
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-10-06 22:18:24 +0300

    Add state persistence to store
    Integrated localStorage to load and save store state. Subscribed to store changes to persist specific state slices. This ensures state is maintained across page reloads.

--- src/App/store.ts
+++ src/App/store.ts
@@ -3,6 +3,7 @@ import {counterReducer} from "features/model/counterReducer/counterReducer.ts";
 import {settingsReducer} from "features/model/settingsReducer/settingsReducer.ts";
 import {errorReducer} from "features/model/errorsReducer/errorReducer.ts";
 import {messageReducer} from "features/model/messagesReducer/messageReducer.ts";
+import {loadState, saveState} from "common/types/localStorage.ts";
 
 const rootReducer = combineReducers({
     counter: counterReducer,
@@ -10,6 +11,20 @@ const rootReducer = combineReducers({
     error: errorReducer,
     messages: messageReducer
 })
-export const store = legacy_createStore(rootReducer);
+
+export const store = legacy_createStore(rootReducer, loadState());
+
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter,
+        settings: {
+            startValue: store.getState().settings.startValue,
+            maxValue: store.getState().settings.maxValue
+        },
+        error: store.getState().error,
+        messages: store.getState().messages
+    })
+})
+
 export type RootState = ReturnType<typeof store.getState>
 export type RootDispatch = typeof store.dispatch
